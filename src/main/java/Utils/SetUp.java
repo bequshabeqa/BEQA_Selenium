22e2fd35f4a4566613b1c1911cadef1b27c1b3e4
bchitadze <beqachita@gmail.com>
2025-01-23 22:01:06 +0400

    23.01.2025

--- src/main/java/Utils/SetUp.java
+++ src/main/java/Utils/SetUp.java
@@ -1,21 +1,19 @@
 package Utils;
 
 import org.testng.annotations.BeforeMethod;
-
 import java.lang.module.Configuration;
-
 import static java.lang.module.Configuration.*;
 
 
 public class SetUp  {
 
-//    public void setUpMethod(){
-//
-//        Configuration.browser = "chrome";
-//    }
+   public void setUpMethod(){
 
-//    @BeforeMethod
-//    public void before() {
-//        setUpMethod();
-//    }
+//        Configuration.browser = "chrome";
+    }
+
+    @BeforeMethod
+    public void before() {
+        setUpMethod();
+    }
 }
